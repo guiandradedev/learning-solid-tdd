77be94a0c939f8413b38789e7a584dad06268fc7
Guilherme Andrade <42524262+guiandradedev@users.noreply.github.com>
2023-07-26 23:53:55 -0300

    test: should throw an error if code is invalid

--- src/application/useCases/user/resetPassword/resetPassword.spec.ts
+++ src/application/useCases/user/resetPassword/resetPassword.spec.ts
@@ -1,11 +1,23 @@
-import { User } from "@/domain/entities";
-import { describe, expect, it } from "vitest";
+import 'reflect-metadata'
+import { User, UserCode } from "@/domain/entities";
+import { describe, expect, it, vitest } from "vitest";
 import { ResetPasswordUseCase } from "./resetPasswordUseCase";
-import { ErrInvalidParam } from "@/shared/errors";
+import { ErrInvalidParam, ErrNotFound } from "@/shared/errors";
+import { InMemoryUserCodeRepository } from '@/tests/repositories';
 
 describe('Reset Password', ()=>{
     it('should reset password', async () => {
-        const sut = new ResetPasswordUseCase()
+        const userCodeRepository = new InMemoryUserCodeRepository()
+        const sut = new ResetPasswordUseCase(userCodeRepository)
+
+        vitest.spyOn(userCodeRepository, 'findByCode').mockReturnValue(new Promise((resolve)=>resolve(UserCode.create({
+            active: true,
+            code: 'VALID_CODE',
+            createdAt: new Date(),
+            expiresIn: new Date(new Date().setHours(new Date().getHours() + 3)),
+            type: "FORGOT_PASSWORD",
+            userId: 'fake_user_id'
+        }))))
 
         const reset = await sut.execute({
             code: "VALID_CODE",
@@ -17,15 +29,16 @@ describe('Reset Password', ()=>{
     })
 
     it('should throw an error if code is invalid', async () => {
-        // const sut = new ResetPasswordUseCase()
+        const userCodeRepository = new InMemoryUserCodeRepository()
+        const sut = new ResetPasswordUseCase(userCodeRepository)
 
-        // const reset = sut.execute({
-        //     code: "invalid_code",
-        //     password: "password",
-        //     confirmPassword: "password"
-        // })
+        const reset = sut.execute({
+            code: "invalid_code",
+            password: "password",
+            confirmPassword: "password"
+        })
 
-        // expect(reset).rejects.toBeInstanceOf(ErrInvalidParam)
+        expect(reset).rejects.toBeInstanceOf(ErrNotFound)
     })
     it('should throw an error if code expired', async () => {})
     it('should throw an error if password and confirm password does not match', async () => {})
